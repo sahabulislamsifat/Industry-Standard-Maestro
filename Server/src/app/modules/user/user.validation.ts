9271f522171bec4733ab345234025fbee4a4250d
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-08-03 01:30:07 +0600

    part 6 complete

--- Server/src/app/modules/user/user.validation.ts
+++ Server/src/app/modules/user/user.validation.ts
@@ -35,11 +35,13 @@ export const createUserZodSchema = z.object({
     })
     .optional(),
 });
+
 export const updateUserZodSchema = z.object({
   name: z
     .string()
     .min(2, { message: "Name must be at least 2 characters long" })
-    .max(50, { message: "Name can't exceed 50 characters" }),
+    .max(50, { message: "Name can't exceed 50 characters" })
+    .optional(),
 
   email: z.string().email({ message: "Invalid email address" }).optional(),
 
@@ -59,7 +61,7 @@ export const updateUserZodSchema = z.object({
 
   role: z.enum(Object.values(UserRole) as [string]).optional(),
 
-  IsActive: z.enum(Object.values(IsActive) as [string]).optional(),
+  isActive: z.enum(Object.values(IsActive) as [string]).optional(),
 
   isDeleted: z.boolean().optional(),
 
@@ -68,5 +70,13 @@ export const updateUserZodSchema = z.object({
   address: z
     .string()
     .min(5, { message: "Address must be at least 5 characters long" })
+    .optional(),
+
+  phone: z
+    .string()
+    .regex(/^(?:\+8801\d{9}|01\d{9})$/, {
+      message:
+        "Phone number must be valid for Bangladesh (e.g., +8801XXXXXXXXX or 01XXXXXXXXX)",
+    })
     .optional(),
 });
