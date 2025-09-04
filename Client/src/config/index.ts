868c0e8e77e7eceaf5f44148014a5586801437dc
Sahabul Islam Sifat <154552049+sahabulislamsifat@users.noreply.github.com>
2025-09-04 20:54:11 +0600

    Delete Client directory

--- Client/src/config/index.ts
+++
@@ -1,5 +0,0 @@
-const config = {
-  baseUrl: import.meta.env.VITE_BASE_URL,
-};
-
-export default config;
--- Client/src/redux/baseApi.ts
+++
@@ -1,8 +0,0 @@
-import { createApi } from "@reduxjs/toolkit/query/react";
-import axiosBaseQuery from "./axiosBaseQuery";
-
-export const baseApi = createApi({
-  reducerPath: "baseApi",
-  baseQuery: axiosBaseQuery(),
-  endpoints: () => ({}),
-});
