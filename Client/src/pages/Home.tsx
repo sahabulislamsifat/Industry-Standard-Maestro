29ba8273bc8c7a3a9bb19c176619ab2a75d24e12
Sahabul Islam Sifat <154552049+sahabulislamsifat@users.noreply.github.com>
2025-09-04 20:54:45 +0600

    Delete Client directory

--- Client/src/pages/Home.tsx
+++
@@ -1,9 +0,0 @@
-const Home = () => {
-  return (
-    <div>
-      <h1>This is Home Component...</h1>
-    </div>
-  );
-};
-
-export default Home;
--- Client/src/pages/admin/Analytics.tsx
+++
@@ -1,9 +0,0 @@
-const Analytics = () => {
-  return (
-    <div>
-      <h1>This is Analytics page for admin </h1>
-    </div>
-  );
-};
-
-export default Analytics;
